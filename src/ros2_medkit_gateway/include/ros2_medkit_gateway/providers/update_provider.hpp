c2691997a2ddfae2fd9485f51a85c9a8e4e2be1a
Bartosz Burda <bartoszburda93@gmail.com>
2026-02-24 17:32:32 +0000

    feat(plugins): add UpdateProvider and IntrospectionProvider interfaces
    Typed provider interfaces that plugins implement alongside GatewayPlugin.
    UpdateProvider mirrors existing UpdateBackend. IntrospectionProvider
    defines the enrichment/discovery interface.
    
    Refs #235

---
+++ src/ros2_medkit_gateway/include/ros2_medkit_gateway/providers/update_provider.hpp
@@ -0,0 +1,54 @@
+// Copyright 2026 bburda
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+// TODO(Task 5): rename this include to update_types.hpp when UpdateBackend is removed
+#include "ros2_medkit_gateway/updates/update_backend.hpp"
+
+namespace ros2_medkit_gateway {
+
+/**
+ * @brief Provider interface for software update backends
+ *
+ * Typed provider interface that plugins implement alongside GatewayPlugin
+ * via multiple inheritance. Replaces the old UpdateBackend abstract class.
+ *
+ * Reuses all types from update_backend.hpp (UpdateFilter, UpdateBackendErrorInfo,
+ * UpdateProgressReporter, etc.).
+ *
+ * @see GatewayPlugin for the base class
+ * @see UpdateManager for the subsystem that uses this
+ */
+class UpdateProvider {
+ public:
+  virtual ~UpdateProvider() = default;
+
+  // ---- CRUD (metadata storage owned by plugin) ----
+  virtual tl::expected<std::vector<std::string>, UpdateBackendErrorInfo> list_updates(const UpdateFilter & filter) = 0;
+  virtual tl::expected<nlohmann::json, UpdateBackendErrorInfo> get_update(const std::string & id) = 0;
+  virtual tl::expected<void, UpdateBackendErrorInfo> register_update(const nlohmann::json & metadata) = 0;
+  virtual tl::expected<void, UpdateBackendErrorInfo> delete_update(const std::string & id) = 0;
+
+  // ---- Async operations (called in background threads) ----
+  virtual tl::expected<void, UpdateBackendErrorInfo> prepare(const std::string & id,
+                                                             UpdateProgressReporter & reporter) = 0;
+  virtual tl::expected<void, UpdateBackendErrorInfo> execute(const std::string & id,
+                                                             UpdateProgressReporter & reporter) = 0;
+
+  // ---- Capability queries ----
+  virtual tl::expected<bool, UpdateBackendErrorInfo> supports_automated(const std::string & id) = 0;
+};
+
+}  // namespace ros2_medkit_gateway
