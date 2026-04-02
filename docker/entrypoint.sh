48624fd2310853145d3c2d635631f05abd36db84
Bartosz Burda <bartoszburda93@gmail.com>
2026-04-02 19:22:37 +0200

    feat: add Dockerfile and GHCR publish workflow for gateway
    Multi-stage Docker build (builder + runtime) with ROS_DISTRO build arg
    for multi-distro support. Includes all open-core packages and plugins.
    
    Workflow publishes 3 images on push to main:
    - ghcr.io/selfpatch/ros2_medkit-jazzy:latest
    - ghcr.io/selfpatch/ros2_medkit-humble:latest
    - ghcr.io/selfpatch/ros2_medkit-rolling:latest
    
    Default config at /etc/ros2_medkit/params.yaml (overridable via volume
    mount). External plugins can be mounted at /opt/ros2_medkit/plugins/.
    
    Closes #342

---
+++ docker/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright 2026 bburda
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+# Source ROS 2 and the built workspace
+# shellcheck disable=SC1090,SC1091
+source "/opt/ros/${ROS_DISTRO}/setup.bash"
+# shellcheck disable=SC1091
+source /root/ws/install/setup.bash
+
+# Default to FastDDS (can be overridden via RMW_IMPLEMENTATION env var)
+export RMW_IMPLEMENTATION="${RMW_IMPLEMENTATION:-rmw_fastrtps_cpp}"
+
+# Pass all arguments to the gateway node
+# Default usage: entrypoint.sh --params-file /etc/ros2_medkit/params.yaml
+# Custom: docker run ros2_medkit --params-file /my/config.yaml --ros-args -p server.port:=9090
+exec ros2 run ros2_medkit_gateway gateway_node --ros-args "$@"
